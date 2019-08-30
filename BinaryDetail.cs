08d41e657c2ecebb3d4b1fd7f7964b938abccf0d
Stuart Smith <stuart.smith@aveva.com>
2019-08-30 14:03:07 +0100

    Added a load of additional detail to the csv report

---
+++ BinaryDetail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace NetVersionChecker
+{
+    internal class BinaryDetail
+    {
+        public BinaryDetail(string fullPath)
+        {
+            FullPath = fullPath;
+            FileName = Path.GetFileName(fullPath);
+        }
+
+        public string FullPath;
+        public string FileName;
+        public string ImageRuntimeVersion = String.Empty;
+        public string TargetFrameworkAttribute = String.Empty;
+        public string Error = String.Empty;
+        public PortableExecutableKinds PEKind;
+        public ImageFileMachine ImageFileMachine;
+        public ProcessorArchitecture ProcessorArchitecture;
+
+        public string ToCsv()
+        {
+            return FullPath + "," + FileName + "," + AssemblyVersion + "," + FileVersion + "," + ProductVersion + "," + ImageRuntimeVersion + "," + TargetFrameworkAttribute + "," + PEKind + "," + ImageFileMachine + "," + ProcessorArchitecture + "," + Error;
+        }
+
+        public static string[] CSVHeader
+        {
+            get
+            {
+                return new[] { "Full Path,FileName,AssemblyVersion,FileVersion,ProductVersion,ImageRuntimeVersion,TargetFrameworkAttribute,PortableExecutableKinds,ImageFileMachine,ProcessorArchitecture,Error" };
+            }
+        }
+
+        public string AssemblyVersion { get; internal set; }
+        public string FileVersion { get; internal set; }
+        public string ProductVersion { get; internal set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -7,28 +8,42 @@ using System.Text.RegularExpressions;
 
 namespace NetVersionChecker
 {
-    class Program
+    internal class Program
     {
-        static string currentBinary = string.Empty;
-        static void Main(string[] args)
+        private static string currentBinary = string.Empty;
+
+        private static void Main(string[] args)
          {
             string path = args[0];
-            List<NetFileCheck> netFileChecks = new List<NetFileCheck>();
+            List<BinaryDetail> binaryDetails = new List<BinaryDetail>();
             Environment.CurrentDirectory = path;
 
             IEnumerable<string> allFiles = GetFiles(path, new[] { "*.dll", "*.exe" }, SearchOption.TopDirectoryOnly);
             AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(CurrentDomain_ReflectionOnlyAssemblyResolve);
-            foreach(string binary in allFiles)
+
+            foreach (string binary in allFiles)
             {
                 currentBinary = binary;
-                NetFileCheck nfc = new NetFileCheck(binary);
-                netFileChecks.Add(nfc);
-                Console.WriteLine(binary);
+                BinaryDetail bd = new BinaryDetail(binary);
+                binaryDetails.Add(bd);
+
                 try
                 {
                     Assembly loadFrom = Assembly.LoadFrom(binary);
-                    Console.WriteLine("ImageRuntimeVersion: " + loadFrom.ImageRuntimeVersion);
-                    nfc.ImageRuntimeVersion = loadFrom.ImageRuntimeVersion;
+
+                    bd.ImageRuntimeVersion = loadFrom.ImageRuntimeVersion;
+                    bd.AssemblyVersion = loadFrom.GetName().Version.ToString();
+                    bd.FileVersion = FileVersionInfo.GetVersionInfo(binary).FileVersion;
+                    bd.ProductVersion = FileVersionInfo.GetVersionInfo(binary).ProductVersion;
+
+                    PortableExecutableKinds peKind;
+                    ImageFileMachine machine;
+                    loadFrom.ManifestModule.GetPEKind(out peKind, out machine);
+                    bd.PEKind = peKind;
+                    bd.ImageFileMachine = machine;
+
+                    ProcessorArchitecture pe = loadFrom.GetName().ProcessorArchitecture;
+                    bd.ProcessorArchitecture = pe;
 
                     Assembly loadFromReflectionOnly = Assembly.ReflectionOnlyLoadFrom(binary);
                     bool TargetFrameworkAttributeFound = false;
@@ -36,8 +51,7 @@ namespace NetVersionChecker
                     {
                         if (att.AttributeType == typeof(System.Runtime.Versioning.TargetFrameworkAttribute))
                         {
-                            Console.WriteLine("TargetFrameworkAttribute:" + att.ConstructorArguments[0].ToString());
-                            nfc.TargetFrameworkAttribute = att.ConstructorArguments[0].ToString();
+                            bd.TargetFrameworkAttribute = att.ConstructorArguments[0].ToString();
                             TargetFrameworkAttributeFound = true;
                             break;
                         }
@@ -45,21 +59,19 @@ namespace NetVersionChecker
                     if(!TargetFrameworkAttributeFound)
                     {
                         string message = "No TargetFrameworkAttribute found on this assembly";
-                        Console.WriteLine(message);
-                        nfc.Error = message;
+                        bd.Error = message;
                     }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
-                    nfc.Error = e.Message;
+                    bd.Error = e.Message;
                 }
-                Console.WriteLine();
             }
+
             string fileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Guid.NewGuid() + ".csv");
             File.Create(fileName).Close();
-            File.AppendAllLines(fileName, new[] { "Full Path,FileName,ImageRuntimeVersion,TargetFrameworkAttribute,Error" });
-            foreach(var nfc in netFileChecks)
+            File.AppendAllLines(fileName, BinaryDetail.CSVHeader);
+            foreach(var nfc in binaryDetails)
             {
                 File.AppendAllLines(fileName, new[] { nfc.ToCsv() });
             }
@@ -67,7 +79,7 @@ namespace NetVersionChecker
             Console.In.ReadLine();
         }
 
-        static Assembly CurrentDomain_ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
+        private static Assembly CurrentDomain_ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
         {
             AssemblyName assemblyName = new AssemblyName(args.Name);
             string assemblyPath = Path.GetDirectoryName(currentBinary) + "\\" + assemblyName.Name + ".dll";
@@ -79,32 +91,13 @@ namespace NetVersionChecker
             return Assembly.ReflectionOnlyLoad(args.Name);
         }
 
-        static IEnumerable<string> GetFiles(string path,
+        private static IEnumerable<string> GetFiles(string path,
                             string[] searchPatterns,
                             SearchOption searchOption = SearchOption.TopDirectoryOnly)
         {
             return searchPatterns.AsParallel()
                    .SelectMany(searchPattern =>
                           Directory.EnumerateFiles(path, searchPattern, searchOption));
-        }
-    }
-
-    class NetFileCheck
-    {
-        public NetFileCheck(string fullPath)
-        {
-            FullPath = fullPath;
-            FileName = Path.GetFileName(fullPath);
-        }
-        public string FullPath;
-        public string FileName;
-        public string ImageRuntimeVersion = String.Empty;
-        public string TargetFrameworkAttribute = String.Empty;
-        public string Error = String.Empty;
-
-        public string ToCsv()
-        {
-            return FullPath + "," + FileName + "," + ImageRuntimeVersion + "," + TargetFrameworkAttribute + "," + Error;
         }
     }
 }
