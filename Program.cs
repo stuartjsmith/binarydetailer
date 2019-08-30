c37cd6ccfb7e6224eaaa86d687bd3f98b277e96a
Stuart Smith <stuart.smith@aveva.com>
2019-08-30 08:54:00 +0100

    Output to folder, more robust reflection only loading

--- Program.cs
+++ Program.cs
@@ -10,18 +10,23 @@ namespace NetVersionChecker
     class Program
     {
          static void Main(string[] args)
-         { 
-
-            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(CurrentDomain_ReflectionOnlyAssemblyResolve);
+         {
             string path = args[0];
+            List<NetFileCheck> netFileChecks = new List<NetFileCheck>();
             Environment.CurrentDirectory = path;
-            foreach(string binary in MyDirectory.GetFiles(path, "\\.dll|\\.exe", SearchOption.AllDirectories))
+
+            IEnumerable<string> allFiles = GetFiles(path, new[] { "*.dll", "*.exe" }, SearchOption.TopDirectoryOnly);
+            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(CurrentDomain_ReflectionOnlyAssemblyResolve);
+            foreach(string binary in allFiles)
             {
+                NetFileCheck nfc = new NetFileCheck(binary);
+                netFileChecks.Add(nfc);
                 Console.WriteLine(binary);
                 try
                 {
                     Assembly loadFrom = Assembly.LoadFrom(binary);
                     Console.WriteLine("ImageRuntimeVersion: " + loadFrom.ImageRuntimeVersion);
+                    nfc.ImageRuntimeVersion = loadFrom.ImageRuntimeVersion;
 
                     Assembly loadFromReflectionOnly = Assembly.ReflectionOnlyLoadFrom(binary);
                     bool TargetFrameworkAttributeFound = false;
@@ -30,51 +35,74 @@ namespace NetVersionChecker
                         if (att.AttributeType == typeof(System.Runtime.Versioning.TargetFrameworkAttribute))
                         {
                             Console.WriteLine("TargetFrameworkAttribute:" + att.ConstructorArguments[0].ToString());
+                            nfc.TargetFrameworkAttribute = att.ConstructorArguments[0].ToString();
                             TargetFrameworkAttributeFound = true;
                             break;
                         }
                     }
                     if(!TargetFrameworkAttributeFound)
                     {
-                        Console.WriteLine("No TargetFrameworkAttribute found on this assembly");
+                        string message = "No TargetFrameworkAttribute found on this assembly";
+                        Console.WriteLine(message);
+                        nfc.Error = message;
                     }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
+                    nfc.Error = e.Message;
                 }
                 Console.WriteLine();
             }
+            string fileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Guid.NewGuid() + ".csv");
+            File.Create(fileName).Close();
+            File.AppendAllLines(fileName, new[] { "Full Path,FileName,ImageRuntimeVersion,TargetFrameworkAttribute,Error" });
+            foreach(var nfc in netFileChecks)
+            {
+                File.AppendAllLines(fileName, new[] { nfc.ToCsv() });
+            }
+            Console.WriteLine("File created at " + fileName);
             Console.In.ReadLine();
         }
 
         static Assembly CurrentDomain_ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
-
         {
-            return System.Reflection.Assembly.ReflectionOnlyLoad(args.Name);
-        }
-    }
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+            string assemblyPath = Environment.CurrentDirectory + "\\" + assemblyName.Name + ".dll";
 
-    public static class MyDirectory
-    {   // Regex version
-        public static IEnumerable<string> GetFiles(string path,
-                            string searchPatternExpression = "",
-                            System.IO.SearchOption searchOption = SearchOption.TopDirectoryOnly)
-        {
-            Regex reSearchPattern = new Regex(searchPatternExpression, RegexOptions.IgnoreCase);
-            return Directory.EnumerateFiles(path, "*", searchOption)
-                            .Where(file =>
-                                     reSearchPattern.IsMatch(Path.GetExtension(file)));
+            if (File.Exists(assemblyPath))
+            {
+                return Assembly.ReflectionOnlyLoadFrom(assemblyPath);
+            }
+            return Assembly.ReflectionOnlyLoad(args.Name);
         }
 
-        // Takes same patterns, and executes in parallel
-        public static IEnumerable<string> GetFiles(string path,
+        static IEnumerable<string> GetFiles(string path,
                             string[] searchPatterns,
                             SearchOption searchOption = SearchOption.TopDirectoryOnly)
         {
             return searchPatterns.AsParallel()
                    .SelectMany(searchPattern =>
                           Directory.EnumerateFiles(path, searchPattern, searchOption));
+        }
+    }
+
+    class NetFileCheck
+    {
+        public NetFileCheck(string fullPath)
+        {
+            FullPath = fullPath;
+            FileName = Path.GetFileName(fullPath);
+        }
+        public string FullPath;
+        public string FileName;
+        public string ImageRuntimeVersion = String.Empty;
+        public string TargetFrameworkAttribute = String.Empty;
+        public string Error = String.Empty;
+
+        public string ToCsv()
+        {
+            return FullPath + "," + FileName + "," + ImageRuntimeVersion + "," + TargetFrameworkAttribute + "," + Error;
         }
     }
 }
